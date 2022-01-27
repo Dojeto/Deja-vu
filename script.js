da4ccb532285bdb3498a1ead39295ef7cc2f8c51
Dojeto <81398258+Dojeto@users.noreply.github.com>
2022-01-27 21:10:19 +0530

    Test

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// let burgur = document.querySelector('.burger');
+// let navbar = document.querySelector('.navbar');
+
+// let btn = document.querySelector('.btn-1');
+// let btn2 = document.querySelector('.btn-2');
+// let btn3 = document.querySelector('.btn-3');
+// let btn4 = document.querySelector('.btn-4');
+
+// let load = document.getElementById('preloader');
+
+// burgur.addEventListener('click',()=>{
+//     navbar.classList.toggle('v-nav');
+// })
+
+// window.addEventListener('load',()=>{
+//     load.style.display = "none";
+// })
+
+// window.addEventListener('load',()=>{
+//     btn.classList.add("button")
+// })
+
+// window.addEventListener('load',()=>{
+//     btn2.classList.add("button")
+// })
+
+// window.addEventListener('load',()=>{
+//     btn3.classList.add("button")
+// })
+
+// window.addEventListener('load',()=>{
+//     btn4.classList.add("button")
+// })
+
+
 let burgur = document.querySelector('.burger');
 let navbar = document.querySelector('.navbar');
 
@@ -12,22 +47,22 @@ burgur.addEventListener('click',()=>{
     navbar.classList.toggle('v-nav');
 })
 
-window.addEventListener('load',()=>{
+setTimeout(()=>{
     load.style.display = "none";
-})
+},3000)
 
-window.addEventListener('load',()=>{
+setTimeout(()=>{
     btn.classList.add("button")
-})
+},3000)
 
-window.addEventListener('load',()=>{
+setTimeout(()=>{
     btn2.classList.add("button")
-})
+},3000)
 
-window.addEventListener('load',()=>{
+setTimeout(()=>{
     btn3.classList.add("button")
-})
+},3000)
 
-window.addEventListener('load',()=>{
+setTimeout(()=>{
     btn4.classList.add("button")
-})
+},3000)
