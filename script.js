9c5d588fd3cbc43921e978b2ba042180717d082e
Dojeto <81398258+Dojeto@users.noreply.github.com>
2022-01-30 17:50:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-// let burgur = document.querySelector('.burger');
-// let navbar = document.querySelector('.navbar');
-
-// let btn = document.querySelector('.btn-1');
-// let btn2 = document.querySelector('.btn-2');
-// let btn3 = document.querySelector('.btn-3');
-// let btn4 = document.querySelector('.btn-4');
-
-// let load = document.getElementById('preloader');
-
-// burgur.addEventListener('click',()=>{
-//     navbar.classList.toggle('v-nav');
-// })
-
-// window.addEventListener('load',()=>{
-//     load.style.display = "none";
-// })
-
-// window.addEventListener('load',()=>{
-//     btn.classList.add("button")
-// })
-
-// window.addEventListener('load',()=>{
-//     btn2.classList.add("button")
-// })
-
-// window.addEventListener('load',()=>{
-//     btn3.classList.add("button")
-// })
-
-// window.addEventListener('load',()=>{
-//     btn4.classList.add("button")
-// })
-
-
 let burgur = document.querySelector('.burger');
 let navbar = document.querySelector('.navbar');
 
