686ba08417ad34a4b5cbf44cb71b8a95b2257f83
Dojeto <81398258+Dojeto@users.noreply.github.com>
2022-03-05 20:11:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,8 @@
 let burgur = document.querySelector('.burger');
 let navbar = document.querySelector('.navbar');
 
-let btn = document.querySelector('.btn-1');
-let btn2 = document.querySelector('.btn-2');
-let btn3 = document.querySelector('.btn-3');
-let btn4 = document.querySelector('.btn-4');
-
-let load = document.getElementById('preloader');
+console.log(burgur);
 
 burgur.addEventListener('click',()=>{
     navbar.classList.toggle('v-nav');
 })
-
-setTimeout(()=>{
-    load.style.display = "none";
-},3000)
-
-setTimeout(()=>{
-    btn.classList.add("button")
-},3000)
-
-setTimeout(()=>{
-    btn2.classList.add("button")
-},3000)
-
-setTimeout(()=>{
-    btn3.classList.add("button")
-},3000)
-
-setTimeout(()=>{
-    btn4.classList.add("button")
-},3000)
